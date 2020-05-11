6afa945fced39df006f1817409d338123313413f
ibrahim <ibrahimkocak93@gmail.com>
2020-05-11 14:20:43 +0300

    Jpa repo switched to Crud repo

--- src/main/java/ibrahimkocak/springbooth2/repository/IRepositoryUser.java
+++ src/main/java/ibrahimkocak/springbooth2/repository/IRepositoryUser.java
@@ -1,7 +1,6 @@
 package ibrahimkocak.springbooth2.repository;
 
 import ibrahimkocak.springbooth2.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
