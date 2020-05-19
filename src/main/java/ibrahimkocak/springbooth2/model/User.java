2e8883a8bf6529ecb46727414e11ab44b003a18d
ibrahim <ibrahimkocak93@gmail.com>
2020-05-19 15:57:01 +0300

    used lombok

--- src/main/java/ibrahimkocak/springbooth2/model/User.java
+++ src/main/java/ibrahimkocak/springbooth2/model/User.java
@@ -1,15 +1,23 @@
 package ibrahimkocak.springbooth2.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Table(name = "user")
-public class User {
+@Getter
+@Setter
+@ToString
+public class User implements Serializable {
 
     @Id
     @GeneratedValue
@@ -23,24 +31,24 @@ public class User {
     private Date creationTime;
     private Date lastUpdateTime;
 
-    public boolean setUser(User user) {
+    public boolean updateUser(User user) {
 
         boolean isUpdated = false;
 
         if (user.name != null && !user.name.equals(name)) {
-            setName(user.name);
+            name = user.name;
             isUpdated = true;
         }
         if (user.surname != null && !user.surname.equals(surname)) {
-            setSurname(user.surname);
+            surname = user.surname;
             isUpdated = true;
         }
         if (user.accountType != null && !user.accountType.equals(accountType)) {
-            setAccountType(user.accountType);
+            accountType = user.accountType;
             isUpdated = true;
         }
         if (user.birthday != null && !user.birthday.equals(birthday)) {
-            setBirthday(user.birthday);
+            birthday = user.birthday;
             isUpdated = true;
         }
         if (isUpdated)
@@ -49,80 +57,20 @@ public class User {
         return isUpdated;
     }
 
-    public Long getId() {
-        return id;
-    }
+    /*
+        public User cloneIt() {
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public AccountType getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(AccountType accountType) {
-        this.accountType = accountType;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-
-        if (birthday != null)
-            this.birthday = (Date) birthday.clone();
-    }
-
-    public Date getCreationTime() {
-        return creationTime;
-    }
-
-    public void setCreationTime(Date creationTime) {
-        this.creationTime = creationTime;
-    }
-
-    public Date getLastUpdateTime() {
-        return lastUpdateTime;
-    }
-
-    public void setLastUpdateTime(Date lastUpdateTime) {
-        this.lastUpdateTime = lastUpdateTime;
-    }
-
-    public User cloneIt() {
-
-        User user = new User();
-        user.id = this.id;
-        user.name = this.name;
-        user.surname = this.surname;
-        user.accountType = this.accountType;
-        user.birthday = this.birthday;
-        user.creationTime = this.creationTime;
-        user.lastUpdateTime = this.lastUpdateTime;
-        return user;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "\nUser[id=%s, name='%s', surname='%s', birthday='%tc' , creationDate='%tc' , accountType='%s']\n",
-                id, surname, name, birthday, creationTime, accountType);
-    }
-
+            User user = new User();
+            user.id = this.id;
+            user.name = this.name;
+            user.surname = this.surname;
+            user.accountType = this.accountType;
+            user.birthday = this.birthday;
+            user.creationTime = this.creationTime;
+            user.lastUpdateTime = this.lastUpdateTime;
+            return user;
+        }
+    */
     public enum AccountType {
         Standard,
         Premium
